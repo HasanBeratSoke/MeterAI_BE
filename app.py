1af200fae4431164312e6d7fec0d56b1cc3b6a2a
HasanBeratSoke <hasanberatsoke@hotmail.com>
2023-06-04 18:17:47 +0300

    deploy

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ import base64
 from ultralytics import YOLO
 from PIL import Image
 import cv2 as cv
+import requests
+
 
 app = Flask(__name__)
   
 global model
 model = YOLO("detectModel.pt")
-
+model.to("cpu")
 
 @app.route('/')
 def index():
@@ -40,64 +42,35 @@ def base():
     img = cv.imread(img_path)
 
     #yolov8 model
-    resultsMeter = model.predict(img)
-    result = resultsMeter[0]
-
+    results = model.predict(img)
+    result = results[0]
+    numberList = []
 
     for box in result.boxes:
       class_id = result.names[box.cls[0].item()]
-      if (class_id == "meter"):
+      if (class_id != "meter"):
         cords = box.xyxy[0].tolist()
         cords = [round(x) for x in cords]
         conf = round(box.conf[0].item(), 2)
-
-        # crop meter
-        meter_crop = img[cords[1]:cords[3], cords[0]:cords[2]]
-        resultsNumber = model.predict(meter_crop)
-        result = resultsNumber[0]
-        print(result.names)
-        numberList = []
-        for box in result.boxes:
-            class_id = result.names[box.cls[0].item()]
-            if (class_id != "meter"):
-                cords = box.xyxy[0].tolist()
-                cords = [round(x) for x in cords]
-                conf = round(box.conf[0].item(), 2)
-                print("Object type:", class_id)
-                print("Coordinates:", cords)
-                print("Probability:", conf)
-                print("---")
-                numberList.append((cords, class_id, conf))
-
-        numberList.sort(key=lambda tup: tup[0][0])
-
-        sorted_class_ids = [class_id for _, class_id, _ in numberList]
-        print(sorted_class_ids)
-        sorted_class_ids_string = ''.join(sorted_class_ids)
-
-        # send response to server (BILAL)
-
-        raw_data = {
-            'MeterId': 3,
-            'IndexValue': sorted_class_ids_string
-
-        }
-
-        json_data = json.dumps(raw_data)
+        numberList.append((cords, class_id, conf))
 
 
-        response = request.post('http://yildizbilal000-001-site1.ftempurl.com/invoice/add', json=json_data)
+    numberList.sort(key=lambda tup: tup[0][0])
+    sorted_class_ids = [class_id for _, class_id, _ in numberList]
 
-        
-        if response.status_code == 200:
-            return 'Veri başariyla gönderildi'
-        else:
-            return 'Veri gönderimi başarisiz'
-        
-        break
-      
-        return jsonify(sorted_class_ids)
-    return jsonify("Meter not found")
+    sorted_class_ids_string = ''.join(sorted_class_ids)
+    # send response to server (BILAL)
+    raw_data = {
+     "meterId": 5,
+     "indexValue": sorted_class_ids_string
+    }
+    print(raw_data)
+
+    
+    response = requests.post('http://yildizbilal000-001-site1.ftempurl.com/invoice/add', json=raw_data)
+    
+
+    return jsonify(response.text)
 
 
 if __name__ == "__main__":
